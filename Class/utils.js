a43aff2b9194e12f471fa168d4741ef852d1a2ed
hugo90082 <hugo90082@gmail.com>
2019-07-25 11:02:55 +0800

    0725 1102

--- Class/utils.js
+++ Class/utils.js
@@ -26,16 +26,30 @@ function checkIDTW(id) {
 return ret;
 
 }
-
-function creaetTWID(){
-
+function createAnswer(n=3){//產生不重複數字
+    let poker = [];
+    for (let i = 0; i < 10; i++) {
+        poker[i] = i;
+    }
+    for (let i = poker.length - 1; i > 0; i--) {
+        let rand = parseInt(Math.random() * (i + 1));
+        [poker[i], poker[rand]] = [poker[rand], poker[i]];
+    }
+    let ret = '';
+    for (let i =0;i<n;i++){
+        ret += poker[i];
+    }
+    return ret;
 }
-function createTWIDByArea(area){
 
-}
-function createTWIDByGender(gender) {
-
-}
-function createTWIDByAll(area,gender) {
-
+function checkAB(ans,gus){
+    let a=0,b=0;
+    for(let i =0;i<gus.length;i++){
+        if(gus.charAt(i)==ans.charAt(i)){
+            a++;
+        } else if (ans.indexOf(gus.charAt(i))>=0){
+            b++;
+        }
+    }
+    return `${a}A${b}B`;
 }
