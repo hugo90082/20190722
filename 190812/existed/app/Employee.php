25b5402e8c62ee728fc06e32a933f7c8219f584f
love6622you <lovemaple0913@gmail.com>
2019-08-13 17:04:47 +0800

    0504

--- 190812/existed/app/Employee.php
+++ 190812/existed/app/Employee.php
@@ -8,7 +8,8 @@ class Employee extends Model
 {
     protected $table = "employees";
     protected $primaryKey = 'employeeId';
-
+    public $timestamps = false;
+    
     function department() {
         return $this->hasOne(Department::class, 'departmentId');
     }
